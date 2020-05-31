246b5b087b6483bc528044cad83dcd8d3bbc95c5
JoongSeokD <ljseokd@gmail.com>
2020-05-31 22:20:52 +0900

    Form Login 인증

--- src/main/java/io/security/basicsecurity/SecurityConfig.java
+++ src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -5,6 +5,15 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
@@ -19,7 +28,23 @@ public class SecurityConfig  extends WebSecurityConfigurerAdapter {
 
         //인증 정책
         http
-                .formLogin();
+                .formLogin()
+                .loginPage("/loginPage")
+                .defaultSuccessUrl("/")
+                .failureUrl("/login")
+                .usernameParameter("userId")
+                .passwordParameter("passwd")
+                .loginProcessingUrl("/login_proc")
+                .successHandler((httpServletRequest, httpServletResponse, authentication) -> {
+                    System.out.println("authentication : " + authentication.getName());
+                    httpServletResponse.sendRedirect("/");
+                })
+                .failureHandler((httpServletRequest, httpServletResponse, e) -> {
+                    System.out.println("exception : " + e.getMessage());
+                    httpServletResponse.sendRedirect("/login");
+                })
+            .permitAll()
+        ;
 
     }
 }
