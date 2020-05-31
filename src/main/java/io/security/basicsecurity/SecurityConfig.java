e634a229da90923e2d1abca218a9b05b8ee25618
JoongSeokD <ljseokd@gmail.com>
2020-05-31 23:04:57 +0900

    Logout 처리, LogoutFilter

--- src/main/java/io/security/basicsecurity/SecurityConfig.java
+++ src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -9,10 +9,13 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @Configuration
@@ -45,6 +48,20 @@ public class SecurityConfig  extends WebSecurityConfigurerAdapter {
                 })
             .permitAll()
         ;
+
+        http
+                .logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/login")
+                .addLogoutHandler((httpServletRequest, httpServletResponse, authentication) -> {
+                    HttpSession session = httpServletRequest.getSession();
+                    session.invalidate();
+                })
+                .logoutSuccessHandler(
+                        (httpServletRequest, httpServletResponse, authentication)
+                                -> httpServletResponse.sendRedirect("/login")
+                )
+                .deleteCookies("remember-me");
 
     }
 }
